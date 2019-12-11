cf69fc0f28224b775fd6bf368d1b74ca7e61047b
Matheus Ferreira <matheus.domingues@etec.sp.gov.br>
2019-12-10 21:23:50 -0300

    Criando pagina de cadastro, formulario e bd

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
+from .forms import PedidoForm
 
-# Create your views here.
-
-def home(request):
-    nome = 'Anna'
-    lista_nomes = ['a', 'b', 'c']
-    args = {
-        'nome' : nome,
-        'lista': lista_nomes,
-    }
-    return render(request, 'home.html', args)
+def home(req):
+    # nome = 'Anna'
+    # lista_nomes = ['a', 'b', 'c']
+    # args = {
+    #     'nome' : nome,
+    #     'lista': lista_nomes,
+    # }
+    return render(req, 'home.html')
 
 def post(req):
     return render(req, 'posts.html')
 
 def cadastro(req):
-    return render(req, 'cadastro.html')
+    form = PedidoForm(req.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg': 'O pedido deu certo!'
+        }
+        return render(req, 'cadastro.html', context)
+    context = {
+        'formulario': form
+    }
+    return render(req, 'cadastro.html', context)
